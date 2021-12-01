4849389ed3c633f1fc454a53afbf470e1c0ae203
Kasil <kasil@loutre.info>
2021-12-01 14:56:30 +0100

    day 01 and lots of deletions

---
+++ src/day01.rs
@@ -0,0 +1,52 @@
+use itertools::{izip, Itertools};
+use crate::*;
+
+pub fn run(input:&str){
+    print_header(1);
+    print_parse();
+    let data = timed_run(||{parser::one_int_per_line(input)});
+    print_part_1();
+    let result1 = timed_run(||{count_increases(&data)});
+    print_result(&result1);
+    print_part_2();
+    let result2 = timed_run(||{part2(&data)});
+    print_result(&result2)
+}
+
+fn count_increases(data:&Vec<i64>)->usize{
+    let mut iter1 = data.iter();
+    iter1.next();
+    iter1.zip(data.iter()).filter(|(a,b)| {a>b}).count()
+}
+
+fn rolling_sum(data:&Vec<i64>)->Vec<i64>{
+    let iter0 = data.iter();
+    let mut iter1 = data.iter();
+    let mut iter2 = data.iter();
+    iter1.next();
+    iter2.next();
+    iter2.next();
+
+    izip!(iter0,iter1,iter2).map(|(a,b,c)|{a+b+c}).collect_vec()
+}
+
+fn part2(data:&Vec<i64>)->usize{
+    let sum = rolling_sum(data);
+    count_increases(&sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day01::{count_increases, part2};
+
+    static EXEMPLE:&'static[i64] = &[199,200,208,210,200,207,240,269,260,263];
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(count_increases(&EXEMPLE.to_vec()), 7);
+    }
+    #[test]
+    fn part2_test(){
+        assert_eq!(part2(&EXEMPLE.to_vec()),5)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 extern crate regex;
 extern crate itertools;
 mod parser;
-
-mod puzzles;
+mod day01;
 
 use std::fs::read_to_string;
 use std::time::Instant;
-use puzzles::*;
 use std::vec::Vec;
 use colored::*;
 
@@ -23,68 +21,35 @@ macro_rules! one_day {
    {
         println!("{}",stringify!($day).bold());
 
-        one_day_with_input!($day, read_to_string(concat!(stringify!($day),".input"))?);
+        let input = read_to_string(concat!(stringify!($day),".input"))?;
+        $day::run(&input);
 
     };
 }
 
-macro_rules! one_day_with_input {
-    ($day:ident,$input:expr) => 
-   {
-        println!("{}",stringify!($day).bold());
-
-        let i_string = $input;
-        print!("     {}","parsing".green().dimmed());
-        let input =timed_run(||{$day::Input::new(&i_string)});
-       
-        let mut part1 = $day::Part1::new(&input);
-        let mut part2 = $day::Part2::new(&input);
-
-        print!("      {}","part 1".green());
-        timed_run(||{part1.resolve();});
-        println!("    {}",part1);
-
-        print!("      {}","part 2".green());
-        timed_run(||{part2.resolve();});
-        println!("    {}",part2);
-        println!("");
-
-    };
+pub fn print_header(num:usize){
+    println!("Day {:2}", num);
 }
-/*
-fn group_tuples<A,B>(mut input: Vec<(A,B)>) -> Vec<(A,Vec<B>)>
-where A:std::cmp::Ord +  Copy,
-B:std::cmp::Ord + Copy,
-{
-    input.sort();
-    let mut ret = Vec::new();
-    let mut cur;
-    let mut it = input.iter();
-    match it.next(){
-        None => return ret,
-        Some(x) => {
-            ret.push( (x.0,Vec::new()));
-            let len = ret.len()-1;
-            cur = &mut ret[len];
-        }
-    }
-    for i in input.iter(){
-        if i.0 == cur.0 {
-            cur.1.push(i.1);
-        }else{
-            ret.push( (i.0,Vec::new()));
-            let len = ret.len()-1;
-            cur = &mut ret[len];
-        }
-    }
-    ret
+
+pub fn print_parse(){
+    print!("   {}","parsing".green().dimmed());
 }
-*/
+
+pub fn print_part_1(){
+    print!("    {}","part 1".green());
+}
+
+pub fn print_part_2(){
+    print!("    {}","part 2".green());
+}
+
+pub fn print_result<T:std::fmt::Display>(result:&T){
+    println!("     {}",result);
+}
 
 fn main() -> Result<(), std::io::Error> {
 //  one_day_with_input!(day15,"3,1,2");
-//  one_day!(day15);
-
+    one_day!(day01);
 
     Ok(())  
 }

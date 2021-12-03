4686945c9c4994f1d3c4909d2473a982e475eb5a
Kasil <kasil@loutre.info>
2021-12-03 15:57:41 +0100

    day 3 complete

--- src/day03.rs
+++ src/day03.rs
@@ -5,7 +5,7 @@ pub fn run()-> Result<(), std::io::Error>{
     print_single_parse(3,
         parser::one_char_vec_per_line,
         part1,
-        part1
+        part2
     )
 }
 
@@ -13,7 +13,7 @@ pub fn run()-> Result<(), std::io::Error>{
 #[cfg(test)]
 mod tests {
     use crate::parser::one_char_vec_per_line;
-    use crate::day03::{part1};
+    use crate::day03::{part1,part2};
 
     static EXEMPLE:&'static str = "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010\n";
 
@@ -22,7 +22,11 @@ mod tests {
         let data =  one_char_vec_per_line(EXEMPLE);
         assert_eq!(part1(&data), format!("gamma {} [{}] epsilon {} [{}] solution {}","10110",22,"01001",9,198));
     }
-
+    #[test]
+    fn part2_test() {
+        let data =  one_char_vec_per_line(EXEMPLE);
+        assert_eq!(part2(&data), format!("oxygen {} [{}] CO2 {} [{}] solution {}","10111",23,"01010",10,230));
+    }
 }
 
 fn part1(data:&Vec<Vec<char>>)->String{
@@ -37,6 +41,40 @@ fn part1(data:&Vec<Vec<char>>)->String{
             else {gamma.push('0');epsilon.push('1');(gamma_dec*2,epsilon_dec*2+1)}
         });
         format!("gamma {} [{}] epsilon {} [{}] solution {}",gamma,gamma_dec,epsilon,epsilon_dec,gamma_dec*epsilon_dec)
+}
+
+fn count_ones_at(data:&Vec<Vec<char>>,skip:usize)->usize{
+    data.iter().fold(0,|tmp,item|{if item[skip] == '1'{tmp+1}else{tmp}})
+}
+
+fn filter_most(data:Vec<Vec<char>>,skip:usize)->Vec<char>{
+    let len = data.len();
+    let c = count_ones_at(&data, skip);
+    let want = if  c*2 >= len{'1'}else{'0'};
+    let ndata:Vec<Vec<char>> = data.into_iter().filter(|x| x[skip] == want).collect::<Vec<Vec<char>>>();
+    if ndata.len() == 1 {ndata[0].clone()}else{filter_most(ndata, skip+1)}
+}
+fn filter_least(data:Vec<Vec<char>>,skip:usize)->Vec<char>{
+    let len = data.len();
+    let c = count_ones_at(&data, skip);
+    let want = if  c*2 < len{'1'}else{'0'};
+    let ndata:Vec<Vec<char>> = data.into_iter().filter(|x| x[skip] == want).collect::<Vec<Vec<char>>>();
+    if ndata.len() == 1 {ndata[0].clone()}else{filter_least(ndata, skip+1)}
+}
+
+fn charvec_to_int(data:&Vec<char>)->i32{
+    data.iter().fold(0,|v,i|v*2+if *i == '1' {1}else{0})
+}
+
+fn part2(data:&Vec<Vec<char>>)->String{
+    let ndata = data.clone();
+    let oxygen = filter_most(ndata, 0);
+    let oxygen_dec = charvec_to_int(&oxygen);
+    let ndata = data.clone();
+    let co2 = filter_least(ndata,0);
+    let co2_dec = charvec_to_int(&co2);
+    format!("oxygen {} [{}] CO2 {} [{}] solution {}",oxygen.iter().collect::<String>(),oxygen_dec,co2.iter().collect::<String>(),co2_dec,oxygen_dec*co2_dec)
+    
 }
 
 fn count_ones_and_zeros(data:&Vec<Vec<char>>)->Vec<usize>{
